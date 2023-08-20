d70fcbe71036a99d38aacc79c9b99541e6ec4196
varenh <bvarenhorst@outlook.com>
2023-08-20 15:18:08 +0200

    start of function

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import os
 import shutil
 
+
+def organize(path): 
+    file_type = {
+        ".jpg": "Images",
+        ".png": "Images",
+        ".gif": "Images",
+        ".pdf": "Documents",
+        ".txt": "Documents",
+        ".mp4": "Videos",
+        ".mov": "Videos",
+    } # file type with correspondig folder
+    
+    file = os.listdir(path)
+
+    #add logic for organizing..
+
+
+if __name__ == "__main__":
+    path = input("Enter path: ")
+    organize(path)
+
