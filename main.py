a4a32aa555e85dd3d2aebd1cae67b7e8ff54e640
varenh <bvarenhorst@outlook.com>
2023-08-21 15:52:05 +0200

    logic added

--- main.py
+++ main.py
@@ -13,9 +13,21 @@ def organize(path):
         ".mov": "Videos",
     } # file type with correspondig folder
     
-    file = os.listdir(path)
+    files = os.listdir(path)
+    for file in files:
+        extension = os.path.splitext(file)[1] # access file extension
 
-    #add logic for organizing..
+        if extension in file_type:
+            category = file_type[extension]
+            category_path = os.path.join(path, category)
+
+            if not os.path.exists(category_path): # create new folder if there isn't one already
+                os.makedirs(category_path)
+        
+            source = os.path.join(path, file)
+            dest = os.path.join(category_path, file)
+
+            shutil.move(source, dest)
 
 
 if __name__ == "__main__":
